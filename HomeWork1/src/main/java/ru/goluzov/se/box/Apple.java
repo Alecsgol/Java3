3caac895117281525e32c423be5b9ec2de789c9f
Alecs <goluzov_a@mail.ru>
2019-02-18 22:38:13 +0300

    HomeWork1

---
+++ HomeWork1/src/main/java/ru/goluzov/se/box/Apple.java
@@ -0,0 +1,8 @@
+package ru.goluzov.se.box;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        super(1.0f);
+    }
+}
---
+++ HomeWork1/src/main/java/ru/goluzov/se/box/Orange.java
@@ -0,0 +1,8 @@
+package ru.goluzov.se.box;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        super(1.5f);
+    }
+}
